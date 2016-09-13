6109836a496217e5e37138d6edde9124c715aa66
levduc <levduc@github.com>
2016-09-13 05:33:01 -0400

    finishing up q3

--- lab1code/q3/checksum.c
+++ lab1code/q3/checksum.c
@@ -5,42 +5,92 @@
 #include <stdint.h>
 #include <fcntl.h>
 #include <inttypes.h>
+#include <sys/stat.h>
 
-// unsigned char checksum (unsigned char *ptr, size_t sz) {
-//     unsigned char chk = 0;
-//     while (sz-- != 0)
-//         chk -= *ptr++;
-//     return chk;
-// }
+off_t fsize(const char *filename) {
+    struct stat st; 
+
+    if (stat(filename, &st) == 0)
+        return st.st_size;
+
+    return -1; 
+}
+
+//convert int 64 to char
+void int64ToChar(unsigned char a[], uint64_t n) {
+  memcpy(a, &n, 8);
+}
+//char to int64
+uint64_t charTo64bitNum(unsigned char a[]) {
+  uint64_t n = 0;
+  memcpy(&n, a, 8);
+  return n;
+}
 
 uint64_t checksum(unsigned char *ptr)
 {
 	int file1;
-    uint64_t checksum = 0; // 64 bit
+    uint64_t csum = 0; // 64 bit
     file1 = open(ptr, O_RDONLY); //read only
     if (file1 >= 0)
 	{
 	    unsigned char c;
 	    while (read(file1, &c, 1) == 1)
 	    {
-	    	checksum += (int64_t) c;
+	    	csum += (int64_t) c;
 	    }    
 	}
 	else
 	{
 		//fail to open		
 	}
-	return checksum;
+	return csum;
 }
 
+int myunchecksum(unsigned char *file1, unsigned char *file2)
+{
+	int f1;
+    f1 = open(file1, O_RDONLY); //read only
+    uint64_t checkf1 = 0; // 64 bit
+    unsigned char checksum1[8]; // 64 bit
+    off_t filesize = fsize(file1);
+    off_t count = 0;
+    if (f1 >= 0 && filesize >= 8)
+	{
+	    unsigned char c;
+	    while (read(f1, &c, 1) == 1)
+	    {
+	    	if(count != filesize - 7)
+	    		checkf1 += (int64_t) c;
+	    	if(count == filesize - 7)
+	    	{
+	    		//checksum1[filesize - count] = c;
+	    		checksum1[filesize - count] = c;
+	    		//printf("%u\n", (unsigned int) c);
+	    	}	
+	    	count += 1;
+	    }    
+	}
+	else
+	{
+		//fail to open	
+		printf("file is too small\n");	
+	}
+	int64_t checkFinal = charTo64bitNum(checksum1);
+	int64_t checkFinal1 = checksum(file2);
+	if(checkFinal1 == checkFinal)
+		return 1;
+	return checkFinal1;
+}
 
 void writeToFile(unsigned char *file1, unsigned char *file2)
 {
 	int f1;
 	int f2;
+	//Open File
 	f1 = open(file1, O_RDONLY);
-	f2 = open(file2, O_CREAT | O_WRONLY);
-	if (f1 >= 0 && f2 >= 0) //open correctly
+	f2 = open(file2, O_CREAT | O_WRONLY | O_TRUNC);
+	if (f1 >= 0 && f2 >= 0)
 	{
 		unsigned char c;
 		while (read(f1, &c, 1) == 1) {
@@ -50,33 +100,38 @@ void writeToFile(unsigned char *file1, unsigned char *file2)
 	else
 	{
 		//fail
+	}    
+    //get checksum of file1
+    uint64_t checksum1 = checksum(file1);
+    unsigned char a[8]; //8bytes
+    int64ToChar(a, checksum1);
+	for (int i = 7; i >=0; i--)
+	{
+		//printf("%u \n", (unsigned int) a[i]);
+		write(f2, &a[i], 1);
 	}
-	uint64_t csFile1 = checksum(file1);
-	printf("%" PRIu64 "\n",csFile1);
-	char cs;
-	while (read(csFile1, &cs, 1) == 1) {
-      write(f2, &cs, 1);
-	}
-
 	close(f1);
 	close(f2);
-}
-
-void int64ToChar(char* a, uint64_t n) {
-  memcpy(a, &n, 8);
 }
 
 int main(int argc, char* argv[])
 {
     uint64_t checksum1 = checksum(argv[1]);
-    char a[sizeof(int64_t)];
-    int64ToChar(a, checksum1);
-	printf("%c",a[1]);
+	printf("%" PRIu64 "\n",checksum1);
+	// printf("%jd\n", (intmax_t) fsize(argv[1]));
+ 	// unsigned char a[sizeof(uint64_t)];
+ 	// int64ToChar(a, checksum1);
+	// printf("%u",(unsigned int) a[0]);
 	// char c;
 	// while (read(a, &c, 1) == 1) {
 	// 	putchar(c);
 	// }
 	//printf("%" PRIu64 "\n",checksum1);
-	//writeToFile(argv[1],argv[2]);
+	writeToFile(argv[1],argv[2]);
+    printf("%d",myunchecksum(argv[2],argv[1]));
+	//uint64_t dcm = 100;
+	//unsigned char a[8];
+ 	//int64ToChar(a, dcm);
+	//printf("%" PRIu64 "\n",charTo64bitNum(a));
     return 0;
 }
