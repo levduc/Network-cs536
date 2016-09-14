eb930bcf1252ed9131317148d24edecb6ba2b165
levduc <levduc@github.com>
2016-09-14 08:37:49 -0400

    add all

--- lab1code/q2/fifoclient.c
+++ lab1code/q2/fifoclient.c
@@ -8,7 +8,8 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-#define MAX_BUF 150
+#define CLIENT_MAX_BUF 150
+#define MAX_BUF 1024
 
 char* concatString(char *s1, char *s2)
 {
@@ -25,26 +26,44 @@ int main(void)
 	int serverFd;
 	int clientFd;
 	char * cmdfifo = "cmdfifo";
-	//char * cfifoPID;
 	int status;
 	int len;
-	char clientRequest[MAX_BUF];
-	char clientBuf[MAX_BUF];
+	char clientRequest[CLIENT_MAX_BUF];
+	char clientBuf[CLIENT_MAX_BUF];
+    char buf[MAX_BUF];
 	while(1){
 		fprintf(stdout,"$");
-		sprintf(clientRequest,"$%d$", getpid());
-		fgets(clientBuf, MAX_BUF, stdin);
+		int pid = getpid();
+		sprintf(clientRequest,"$%d$", pid);
+		fgets(clientBuf, CLIENT_MAX_BUF, stdin);
 		len = strlen(clientBuf);
 		clientBuf[len-1] = '\0';
 		char * IDRequest = concatString(clientRequest,clientBuf);
 		//create client FIFO cmdfifo request
-		status = mkfifo(cmdfifo,0666);
+		//status = mkfifo(cmdfifo,0666);
+		mkfifo(cmdfifo,0666);
 		clientFd = open(cmdfifo, O_WRONLY);
 		write(clientFd, IDRequest, strlen(IDRequest));
 		fprintf(stdout,"[%s] is sent to server \n", IDRequest);
 		free(IDRequest);
-		unlink(cmdfifo);
+		// unlink(cmdfifo);
 		close(clientFd);
+		//create client FIFO cmd cfifoPID
+		// char cfifopid[CLIENT_MAX_BUF];
+		// sprintf(cfifopid,"cfifo%d", pid);
+		// printf("%s\n", cfifopid);
+		// mkfifo(cfifopid,0666);
+		// serverFd = open(cfifopid, O_RDONLY);
+		// if(fd >= 0)
+		// {
+		//     read(serverFd, buf, MAX_BUF);
+		//     printf("Server Response: %s\n", buf);
+		//     close(serverFd);
+		// }
+		// else{
+
+		// 	//fail
+		// }
 	 }
 	return 0;
 }
--- lab1code/q2/fifoserver.c
+++ lab1code/q2/fifoserver.c
@@ -8,24 +8,49 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-#define MAX_BUF 150
+#define CLIENT_MAX_BUF 150
+
 
 int main()
 {
-    int fd;
+    int clientRequestDF;
     char * cmdfifo = "cmdfifo";
-    char buf[MAX_BUF];
     /* open, read, and display the message from the FIFO */
+    char buf[CLIENT_MAX_BUF];
+	char pid[31];
+	char command[31];
 	while(1){
-		fd = open(cmdfifo, O_RDONLY);
-		if(fd >= 0)
+		clientRequestDF = open(cmdfifo, O_RDONLY);
+		if(clientRequestDF > 0) // there is request
 		{
-		    read(fd, buf, MAX_BUF);
-		    printf("Received: %s\n", buf);
-		    close(fd);
+		    read(clientRequestDF, buf, CLIENT_MAX_BUF);
+		    int lens = strlen(buf);
+		    printf("%d\n", lens);		    
+		    //delimiter
+		    const char s[2] = "$";
+		    //get the first token
+		    char *token;
+		    token = strtok(buf, s); 
+			strncpy(pid, token, 30);
+			pid[30] = '\0';
+		    token = strtok(NULL, s);
+			strncpy(command, token, 30);
+			pid[30] = '\0';
+			while(token != NULL)
+			{
+				token = strtok(NULL, s);
+			}
+		    printf("Received From: %s\n", buf);
+		    printf("Received From: %s\n", pid);
+		    printf("Received From: %s\n", command);
+		    close(clientRequestDF);
+		    memset(buf,0, CLIENT_MAX_BUF);
+		    memset(pid,0, CLIENT_MAX_BUF);
+		    memset(command,0, CLIENT_MAX_BUF);
 		}
-		else
+		else{
 			continue;
+		}
 	}
     //return 0;
 }
