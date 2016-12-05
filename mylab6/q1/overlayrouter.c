ad4d6b3979b8ab42c4599bc8cc21d7684b156467
levduc <levduc@github.com>
2016-12-04 22:29:25 -0500

    test

--- mylab6/q1/overlayrouter.c
+++ mylab6/q1/overlayrouter.c
@@ -397,7 +397,7 @@ int main(int argc, char *argv[])
 						printf("Child: Fail to send\n");
 						exit(1);
 					}
-					printf("%s %s\n", inet_ntoa(snd_in.sin_addr, ipForward);
+					printf("%s %s\n", inet_ntoa(snd_in.sin_addr), ipForward);
 					printf("From: [%s:%d] To: [%s:%d]. Timestamp: %d:%d:%d\n"
 							, inet_ntoa(snd_in.sin_addr),ntohs(snd_in.sin_port)
 							, inet_ntoa(csin.sin_addr),ntohs(csin.sin_port)
@@ -417,8 +417,7 @@ int main(int argc, char *argv[])
 						printf("Child: Fail to send\n");
 						exit(1);
 					}
-
-					printf("%s %s\n", inet_ntoa(snd_in.sin_addr, ipForward);
+					printf("%s %s\n", inet_ntoa(snd_in.sin_addr), ipForward);
 					printf("From: [%s:%d] To: [%s:%d]. Timestamp: %d:%d:%d\n"
 							, inet_ntoa(csin.sin_addr),ntohs(csin.sin_port)
 							, ipForward,ntohs(forwardSin.sin_port)
