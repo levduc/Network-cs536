9e473b2deec48dd76ebd9b8582acfe2d9d7f2f17
levduc <levduc@github.com>
2017-02-01 02:32:11 -0500

    modifies server.py. working on concurrent server

--- spring17/lab1/server.py
+++ spring17/lab1/server.py
@@ -1,8 +1,9 @@
 from socket import *;
-import sys;
 from BaseHTTPServer import BaseHTTPRequestHandler
 from StringIO import StringIO
+import sys
 
+#parsing HTTP request method
 class HTTPRequest(BaseHTTPRequestHandler):
     def __init__(self, request_text):
         self.rfile = StringIO(request_text)
@@ -14,48 +15,79 @@ class HTTPRequest(BaseHTTPRequestHandler):
         self.error_code = code
         self.error_message = message
 
-#creating the tcp socket
-serverSocket = socket(AF_INET, SOCK_STREAM);
 
+#creating the tcp socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
 #checking user input
 if len(sys.argv) < 2:
     print 'Usage: python server.py <port-number>'
     exit(1)
+port = int(sys.argv[1])
 
 #binding socket to current address
-host = gethostname();
-port = int(sys.argv[1]);
 #try to bind address
-
 try:
-    serverSocket.bind((host,port));
+    serverSocket.bind(('',port))
 except Exception as e:
     raise e
-    print 'fail to bind';
-    exit(1);
+    print 'fail to bind'
+    exit(1)
 
-serverSocket.listen(1);
-print 'Server is listening';
+serverSocket.listen(1)
+print 'Server is listening'
 
 while True:
     #accepting connection from incoming client
-    connectionSocket,addr = serverSocket.accept();
+    connectionSocket,addr = serverSocket.accept()
     #fork new thread/process to handle
     #receiving GET request
-    recvBuffer = connectionSocket.recv(1024).decode();
-    print recvBuffer;
-    #Parse http request.
-    request = HTTPRequest(recvBuffer);
-    print request.error_code       # None  (check this first)
-    print request.command          # "GET"
-    print request.path             # "/who/ken/trust.html"
-    print request.request_version  # "HTTP/1.1"
-    print len(request.headers)     # 3
-    print request.headers.keys()   # ['accept-charset', 'host', 'accept']
-    print request.headers['host']  # "cm.bell-labs.com"
+    recvBuffer = connectionSocket.recv(1024).decode()
+    print recvBuffer
+    
+    #parse http request.
+    request = HTTPRequest(recvBuffer)
+    #if there is error
+    if request.error_code is not None:                   # None  (check this first)
+        print request.error_code
+        print 'bad HTTP request'
+        exit(1)                                          # exit
+    
+    #check if this is GET request
+    if request.command != "GET":          # "GET"
+        print "Cannot handle " + request.command + " request" 
+        exit(1)
+
+    print request.request_version                        # "HTTP/1.1"
+    #if request.path is / then set it to 'index.html'
+    if request.path == "/":
+        request.path = "/index.html"
+        print request.path
+    #try to open file
+    try:
+        fileToOpen = open("upload" + request.path, "rb")
+    except Exception as e:
+        # raise e
+        message =  'File may not exist.'
+        print message
+        connectionSocket.send(message)
+        continue
+    #start reading + sending file
+    byteRead = fileToOpen.read(20);
+    while len(byteRead) != 0:
+        connectionSocket.send(byteRead)
+        byteRead = fileToOpen.read(20)
+    #close file
+    fileToOpen.close()
+
+    # print len(request.headers)     # 3
+    # print request.headers.keys()   
+    # print request.headers['host']  
 
     #type of Request
-
-    #Check if request is wellformed or not
     #
-    connectionSocket.close();
+
+    #check if request is wellformed or not
+    #
+
+    #closing connection Socket
+    connectionSocket.close()
