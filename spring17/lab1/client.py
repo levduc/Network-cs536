d01de91990c096207521f54becb60d924fa0780c
levduc <levduc@github.com>
2017-02-01 18:21:44 -0500

    multithreading

--- spring17/lab1/client.py
+++ spring17/lab1/client.py
@@ -1,18 +1,73 @@
-from socket import *;
-serverName = 'diamondfromash'
-serverPort = 12001
+from socket import *
+from httplib import HTTPResponse
+from StringIO import StringIO
+import sys
+#this is to manually processing httpresponse
+class modifiedHTTPReponse():
+    def __init__(self, response_str):
+        self._file = StringIO(response_str)
+    def makefile(self, *args, **kwargs):
+        return self._file
+
+# source = FakeSocket(http_response_str)
+# response = HTTPResponse(source)
+# response.begin()
+# print "status:", response.status
+# print "single header:", response.getheader('Content-Type')
+# print "content:", response.read(len(http_response_str)) # the len here will give a 'big enough' value to read the whole content
 
 #creating client socket
 clientSocket = socket(AF_INET,SOCK_STREAM)
 
-#connecting to server
+#checking user input
+if len(sys.argv) < 4:
+    print 'Usage: python client.py <hostname> <hostport> <filename>'
+    exit(1)
+
+#parsing user input
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
+fileName = sys.argv[3]
+# change "/" to "index.html"
+if fileName == "/":
+    fileName = "index.html"
+
+#try to connect to server
 try:
-	clientSocket.connect((serverName,serverPort))
+    clientSocket.connect((serverName,serverPort))
 except Exception as e:
-	raise e
+    raise e
+#create http request and try to send
+httpRequest = "GET /" + fileName + " HTTP/1.0\r\nHost: "+gethostname()+"\r\n\r\n"
+try:
+    clientSocket.send(httpRequest.encode())
+except Exception as e:
+    clientSocket.close()
+    raise e
 
-message = raw_input('Type your message:')
+#wait for response server
+firstRes= clientSocket.recv(1024)
+processedFirstRes = modifiedHTTPReponse(firstRes)
+httpRes = HTTPResponse(processedFirstRes)
+httpRes.begin()
+if httpRes.status != 200:
+    print 'Status code: '+ str(httpRes.status)+ '. Closing connection'
+    clientSocket.close()
+    exit(1)
 
-clientSocket.send(message.encode())
+#try to create file
+try:
+    file = open("Download/"+fileName, "wb")
+    file.write(httpRes.read(len(firstRes)))
+except Exception as e:
+    print '[Error] Fail to create file. Closing connection' 
+    clientSocket.close()
 
+#appending to file
+followRes = clientSocket.recv(1024)
+while len(followRes) != 0:
+    file.write(followRes)
+    followRes = clientSocket.recv(1024)
+print('download completed.')    
+file.close()
 clientSocket.close()
--- spring17/lab1/server.py
+++ spring17/lab1/server.py
@@ -2,8 +2,10 @@ from socket import *;
 from BaseHTTPServer import BaseHTTPRequestHandler
 from StringIO import StringIO
 import sys
+import threading
+import time
 
-#parsing HTTP request method
+#parsing HTTP request class
 class HTTPRequest(BaseHTTPRequestHandler):
     def __init__(self, request_text):
         self.rfile = StringIO(request_text)
@@ -15,79 +17,131 @@ class HTTPRequest(BaseHTTPRequestHandler):
         self.error_code = code
         self.error_message = message
 
+class myThread(threading.Thread):
+    def __init__(self,threadID, connectionSocket):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.connectionSocket = connectionSocket
+    def run(self):
+        recvBuffer = self.connectionSocket.recv(1024).decode()
+        print "============================Thread-"+str(self.threadID)+"=================================="
+        print recvBuffer
+        #parse http request.
+        request = HTTPRequest(recvBuffer)
+        if request.error_code is not None:                   # None  (check this first)
+            print request.error_code
+            print '[Error] Bad HTTP request. Closing connection.'
+            #bad request response
+            message = "HTTP/1.0 400 Bad Request \r\nConnection: close\r\n\r\n"
+            self.connectionSocket.send(message)
+            self.connectionSocket.close()
+            exit(1)
+            
+        if request.command != "GET":                         # "GET"
+            print "[Error] Cannot handle " + str(request.command) + " request" 
+            self.connectionSocket.close()
+            exit(1)
+        time.sleep(5)
+        # print request.request_version + "\r\n"             # "HTTP/1.1"
 
-#creating the tcp socket
-serverSocket = socket(AF_INET, SOCK_STREAM)
-#checking user input
-if len(sys.argv) < 2:
-    print 'Usage: python server.py <port-number>'
-    exit(1)
-port = int(sys.argv[1])
+        #if request.path is / then set it to '/index.html'
+        if request.path == "/":
+            request.path = "/index.html"
 
-#binding socket to current address
-#try to bind address
-try:
-    serverSocket.bind(('',port))
-except Exception as e:
-    raise e
-    print 'fail to bind'
-    exit(1)
+        #try to open file
+        try:
+            fileToOpen = open("Upload" + request.path, "rb")
+        except Exception as e:
+            print '[Error] Fail to open. Fail may not exist or be accessable.'
+            message = 'HTTP/1.0 404 Not Found\r\nConnection: close\r\n\r\n'
+            self.connectionSocket.send(message)
+            self.connectionSocket.close()
+            exit(1)
+        #sending file
+        byteRead = 'HTTP/1.0 200 OK\r\nConnection: close\r\n\r\n'+fileToOpen.read();
+        self.connectionSocket.send(byteRead)
+        #close file
+        fileToOpen.close()
+        print 'Done!'
+        #closing connection Socket
+        self.connectionSocket.close()
+        print 'Exiting ' + str(self.threadID)
 
-serverSocket.listen(1)
-print 'Server is listening'
-
-while True:
-    #accepting connection from incoming client
-    connectionSocket,addr = serverSocket.accept()
-    #fork new thread/process to handle
-    #receiving GET request
-    recvBuffer = connectionSocket.recv(1024).decode()
-    print recvBuffer
-    
-    #parse http request.
-    request = HTTPRequest(recvBuffer)
-    #if there is error
-    if request.error_code is not None:                   # None  (check this first)
-        print request.error_code
-        print 'bad HTTP request'
-        exit(1)                                          # exit
-    
-    #check if this is GET request
-    if request.command != "GET":          # "GET"
-        print "Cannot handle " + request.command + " request" 
+def main():
+    #creating the tcp socket
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    #checking user input
+    if len(sys.argv) < 2:
+        print 'Usage: python server.py <port-number>'
         exit(1)
+    port = int(sys.argv[1])
 
-    print request.request_version                        # "HTTP/1.1"
-    #if request.path is / then set it to 'index.html'
-    if request.path == "/":
-        request.path = "/index.html"
-        print request.path
-    #try to open file
+    #binding socket to current address
+    #try to bind address
     try:
-        fileToOpen = open("upload" + request.path, "rb")
+        serverSocket.bind(('',port))
     except Exception as e:
-        # raise e
-        message =  'File may not exist.'
-        print message
-        connectionSocket.send(message)
-        continue
-    #start reading + sending file
-    byteRead = fileToOpen.read(20);
-    while len(byteRead) != 0:
-        connectionSocket.send(byteRead)
-        byteRead = fileToOpen.read(20)
-    #close file
-    fileToOpen.close()
+        raise e
+        print 'fail to bind'
+        exit(1)
+    serverSocket.listen(5)
+    print 'Server is listening'
+    threadID = 0
+    while True:
+        #accepting connection from incoming client
+        connectionSocket,addr = serverSocket.accept()
+        #fork new thread/process to handle
+        thread = myThread(threadID,connectionSocket)
+        thread.start()
+        threadID += 1
 
-    # print len(request.headers)     # 3
-    # print request.headers.keys()   
-    # print request.headers['host']  
+        # receiving GET request
+        # recvBuffer = connectionSocket.recv(1024).decode()
+        # print "============================HTTP-Request=================================="
+        # print recvBuffer
+        # print "=========================================================================="
+        # #parse http request.
+        # request = HTTPRequest(recvBuffer)
+        # # print len(request.headers)
+        # # print request.headers.keys()
+        # # print request.headers['host']
+        # #if there is error
+        # if request.error_code is not None:                   # None  (check this first)
+        #     print request.error_code
+        #     print '[Error] Bad HTTP request. Closing connection.'
+        #     #bad request response
+        #     message = "HTTP/1.0 400 Bad Request \r\nConnection: close\r\n\r\n"
+        #     connectionSocket.send(message)
+        #     connectionSocket.close()
+        #     continue
+        
+        # #check if this is GET request
+        # if request.command != "GET":                         # "GET"
+        #     print "[Error] Cannot handle " + request.command + " request" 
+        #     connectionSocket.close()
+        #     continue        
 
-    #type of Request
-    #
+        # # print request.request_version + "\r\n"             # "HTTP/1.1"
+        # #if request.path is / then set it to '/index.html'
+        # if request.path == "/":
+        #     request.path = "/index.html"
 
-    #check if request is wellformed or not
-    #
-
-    #closing connection Socket
-    connectionSocket.close()
+        # #try to open file
+        # try:
+        #     fileToOpen = open("Upload" + request.path, "rb")
+        # except Exception as e:
+        #     print '[Error] Fail to open. Fail may not exist or be accessable.'
+        #     message = 'HTTP/1.0 404 Not Found\r\nConnection: close\r\n\r\n'
+        #     connectionSocket.send(message)
+        #     connectionSocket.close()
+        #     continue
+        # #sending file
+        # byteRead = 'HTTP/1.0 200 OK\r\nConnection: close\r\n\r\n'+fileToOpen.read();
+        # connectionSocket.send(byteRead)
+        # #close file
+        # fileToOpen.close()
+        # print 'Done!'
+        # #closing connection Socket
+        # connectionSocket.close()
+        # #end of while loop
+main()
